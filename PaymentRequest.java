6b8e5399a1afc6abb3c9dbb9fe7885cf54b50265
Merveille-umugwaneza <umugwanezamr5@gmail.com>
2026-05-23 22:20:58 +0200

    Create PaymentRequest.java

---
+++ PaymentRequest.java
@@ -0,0 +1,20 @@
+package com.example.idempotencygateway.controllers;
+
+public class PaymentRequest {
+    private int amount;
+    private String currency;
+
+    public int getAmount() {
+        return amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
